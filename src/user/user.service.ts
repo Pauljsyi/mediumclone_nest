568bcc4a380a3e9bc9a3e563275ea8fd1f441a29
Paul Yi <pauljsyi@gmail.com>
2023-08-13 15:46:02 -0600

    implement JWT token for user authentication

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,8 @@ import { CreateUserDto } from './dto/createUser.dto';
 import { UserEntity } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '@app/config';
 
 @Injectable()
 export class UserService {
@@ -10,9 +12,23 @@ export class UserService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
   ) {}
-  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+  async createUser(createUserDto: CreateUserDto): Promise<any> {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+  generateJwt(user: UserEntity): any {
+    return sign(
+      { id: user.id, username: user.username, email: user.email },
+      JWT_SECRET,
+    );
+  }
+  buildUserResponse(user: UserEntity): any {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
